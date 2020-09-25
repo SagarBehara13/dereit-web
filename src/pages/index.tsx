371315194ef2ddafcba5a2b819b3a7883b4a4df7
Adam Azad <adam@adamazad.com>
2020-09-25 18:00:22 +0300

    ♻ Redesign homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,68 @@
+import React from 'react'
 import { Link } from 'gatsby'
 import SEO from '../components/seo'
-import { Center as Layout } from '../layouts'
+import { Flex, Box, Text } from 'rebass/styled-components'
+import { HeaderWithContent as Layout } from '../layouts'
+import Header from '../components/header'
 
 const IndexPage: React.FC = () => {
   return (
-    <Layout width="100vw" height="100vh">
+    <Layout
+      sx={{
+        backgroundImage: `url(${require('../images/home-splash.jpg')})`,
+        backgroundSize: 'cover',
+      }}
+      height="100vh"
+    >
       <SEO title="Home" />
-      <h1>Experience real estate as stock options</h1>
-      <Link to="/expore">Explore</Link>
+      <Layout.Header>
+        <Header />
+      </Layout.Header>
+      <Layout.Content>
+        <Flex
+          alignItems="center"
+          sx={{
+            px: 32,
+          }}
+          width="100%"
+        >
+          <Box>
+            <Box>
+              <Text
+                as="h1"
+                fontSize={
+                  ['8vw', null, '6vw', '4vw', '4vw'] // 38px, null, 80px, null, 156px
+                }
+                sx={{
+                  position: 'relative',
+                  color: 'white',
+                }}
+                display="inline-block"
+                fontFamily="serif"
+                fontWeight="600"
+              >
+                Invest, earn, and grow.
+              </Text>
+            </Box>
+            <Link to="/explore">
+              <Text
+                as="h1"
+                fontSize={
+                  ['8vw', null, '6vw', '4vw', '4vw'] // 38px, null, 80px, null, 156px
+                }
+                sx={{
+                  color: 'white',
+                  position: 'relative',
+                  textDecoration: 'underline',
+                }}
+                display="inline-block"
+              >
+                Explore
+              </Text>
+            </Link>
+          </Box>
+        </Flex>
+      </Layout.Content>
     </Layout>
   )
 }
