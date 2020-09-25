9a0fa05684012ca8c2185f6782893df8855e0eb5
Adam Azad <adam@adamazad.com>
2020-09-25 16:24:03 +0300

    ♻ 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { Center as Layout } from '../layouts'
 import SEO from '../components/seo'
+import { Text } from 'rebass/styled-components'
 
 const NotFoundPage = () => (
   <Layout height="100vh" width="100vw">
     <SEO title="Not found" />
-    <h1>Lost your map?</h1>
-    <p>
-      We have an extra copy for you. <Link to="/">Go back to home</Link>
-    </p>
+    <Text as="h1">Not Found</Text>
+    <Text>
+      <Link to="/">Go back to home</Link>
+    </Text>
   </Layout>
 )
 
