1a2ceb1dd510cccf05620b76e30e7ecbd7ce4f44
Adam Azad <adam@adamazad.com>
2020-09-25 18:00:56 +0300

    ✨ Add details

--- src/components/Property.tsx
+++ src/components/Property.tsx
@@ -1,5 +1,6 @@
-import React, { ComponentProps } from 'react'
-import { Box } from 'rebass'
+import React from 'react'
+import { Box, Text, Image, Flex } from 'rebass/styled-components'
+import { P } from '../components/P'
 
 type Owner = {
   name: string
@@ -37,8 +38,58 @@ interface Props {
   property: Property
 }
 
-const Property: React.FC<Props> = () => {
-  return <Box></Box>
+const Property: React.FC<Props> = ({ property }) => {
+  const pricePerMeter = property.closePrice / property.surfaceArea
+  const rentPerMonth = property.expectedRent
+
+  return (
+    <Flex
+      mb="10px"
+      sx={{
+        minHeight: 200,
+        paddingY: 10,
+        paddingX: 20,
+        borderBottom: '1px solid #ddd',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'relative',
+          overflow: 'hidden',
+          flexBasis: '200px',
+        }}
+      >
+        <Image
+          src={property.images[0]}
+          sx={{
+            position: 'absolute',
+            width: '100%',
+            maxWidth: 'unset',
+            margin: 'auto',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+        />
+      </Box>
+      <Flex
+        flexDirection="column"
+        sx={{
+          flexGrow: 2,
+          paddingLeft: 10,
+        }}
+      >
+        <Text as="h3">{property.name}</Text>
+        <P>
+          <strong>Area</strong> {property.surfaceArea}m<sup>2</sup>
+        </P>
+        <P>
+          <strong>Price per Meter</strong> {pricePerMeter}
+        </P>
+      </Flex>
+    </Flex>
+  )
 }
 
 export default Property
