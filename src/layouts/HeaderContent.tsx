424b139fa3a9e483597859b7f0bd8d2db3e9cb7c
Adam Azad <adam@adamazad.com>
2020-09-25 17:59:54 +0300

    ♻ Explicit control

--- src/layouts/HeaderContent.tsx
+++ src/layouts/HeaderContent.tsx
@@ -1,40 +1,33 @@
 import React from 'react'
+import { Box, BoxProps, Flex, FlexProps } from 'rebass/styled-components'
 import Styled from 'styled-components'
 
 const HEADER_HEIGHT = 60
 
-type ContainerOptions = {
-  height: string | '100%' | '100vh' | 'auto'
-  width: string | '100%' | '100vw' | 'auto'
-}
-
-const Container = Styled.div(
-  (
-    props: ContainerOptions = {
-      height: 'auto',
-      width: 'auto',
-    }
-  ) => `
-  display: flex;
-  min-width: ${props.width};
-  min-height: ${props.height};
-  flex-direction: column;
-`
+const Container = props => (
+  <Box
+    {...props}
+    sx={{
+      ...props.sx,
+      display: 'flex',
+      flexDirection: 'column',
+    }}
+  />
 )
-export const Content = Styled.div`
-  display: flex;
-  flex-grow: 1;
-`
+
+export const Content: React.FC<FlexProps> = props => (
+  <Flex flexGrow={1} {...props} />
+)
 
 export const Header = Styled.div`
-  flex: 0 0 ${HEADER_HEIGHT};
+  flex: 0 0 ${HEADER_HEIGHT}px;
 `
 
-export default class HeaderAndContent extends React.Component {
+export default class HeaderAndContent extends React.Component<BoxProps> {
   static Header = Header
   static Content = Content
 
   render() {
-    return <Container>{this.props.children}</Container>
+    return <Container {...this.props} />
   }
 }
